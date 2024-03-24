3d1e2dee221f6f20c35785d790c894d32ebc7539
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-03-24 22:15:08 +0200

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 import React from 'react';
 import MainPage from './MainPage/MainPage';
 import MatrixEffect from './MatrixTheme/MatrixEffect';
-
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import Wargames from './Wargames/Wargames';
 
 export default function App() {
   return (
     <>
-      <MainPage />
-      <MatrixEffect />
+    <BrowserRouter>
+    <MatrixEffect />
+      <Routes>
+        <Route path="/*" element={<MainPage />} />
+        <Route path="/Wargames" element={<Wargames />} />
+      </Routes>
+    </BrowserRouter>   
     </>
   );
 }
