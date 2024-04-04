34ae056d3a32fc185c8a6199d4ddfdee4ddeaf57
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-04-04 22:50:55 +0300

    Added wargames

--- src/Wargames/Wargames.jsx
+++ src/Wargames/Wargames.jsx
@@ -3,6 +3,7 @@ import "./Wargames.css";
 import TiltCard from "../TiltCard/TiltCard";
 import otw_img from "../assets/cat2.png";
 import sts_img from "../assets/sts.png";
+import pico_img from "../assets/picoctf.png";
 import { Link } from "react-router-dom";
 import ΒackButton from "../BackToMain/BackButton";
 
@@ -23,6 +24,12 @@ const Wargames = () => {
             <Link to="https://overthewire.org/wargames/leviathan/" target="_blank" rel="noopener noreferrer"><TiltCard image={otw_img} title="Leviathan" description="Binary Exploitation"/></Link>
             <Link to="https://overthewire.org/wargames/utumno/" target="_blank" rel="noopener noreferrer"><TiltCard image={otw_img} title="Otumno" description="Binary Exploitation"/></Link>
             <Link to="https://www.smashthestack.org/blowfish.html" target="_blank" rel="noopener noreferrer"><TiltCard image={sts_img} title="Blowfish" description="Binary Exploitation"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=1" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="Web Exploitation"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=2" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="Cryptography"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=3" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="Reverse Engineering"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=4" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="Forensics"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=5" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="General Skills"/></Link>
+            <Link to="https://play.picoctf.org/practice?category=6" target="_blank" rel="noopener noreferrer"><TiltCard image={pico_img} title="PicoCTF" description="Binary Exploitation"/></Link>
             </div>
         </div>
         </>
