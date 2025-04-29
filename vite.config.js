d0772017fe22a1b47b3502b53403d670596b3deb
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-04-29 15:09:20 +0300

    update paths

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   test:{
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/tests/setupTests.ts",
+    setupFiles: "./src/setupTests.ts",
     css: true
   }
 })
